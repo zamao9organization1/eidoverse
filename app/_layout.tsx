d80c90cc6e6b24aa48f37241548c5251a2e05488
uno <zama.barsaev@gmail.com>
2025-08-26 21:43:47 +0300

    feat(core): integrate font loading and splash screen control
    - UseFonts now loads Nunito variants
    - Call SplashScreen.preventAutoHideAsync() on app start
    - Implement useEffect to hide splash screen after fonts load
    - Return null during loading to prevent flicker

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,26 @@
-import { Stack } from "expo-router";
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync(); // Preventing automatic hiding of the splash screen
 
 export default function RootLayout() {
-  return <Stack />;
+	const [loaded] = useFonts({
+		'Nunito-Regular': require('../assets/fonts/Nunito-Regular.ttf'),
+		'Nunito-SemiBold': require('../assets/fonts/Nunito-SemiBold.ttf'),
+		'Nunito-ExtraBold': require('../assets/fonts/Nunito-ExtraBold.ttf'),
+	}); // Fonts
+
+	useEffect(() => {
+		if (loaded) {
+			SplashScreen.hideAsync(); //  Hiding of the splash screen
+		}
+	}, [loaded]);
+
+	if (!loaded) {
+		return null; // Show custom loader while loading fonts
+	}
+
+	return <Stack />;
 }

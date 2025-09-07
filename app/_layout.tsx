244f54db255486b4dcd5c95dd4cbe2dbb6ef78e6
uno <zama.barsaev@gmail.com>
2025-09-07 19:19:33 +0300

    refactor(layout): update root _layout.tsx to support new tab structure
    - Updated app/_layout.tsx to align with new tabs layout
    - Removed legacy navigation logic
    - Improved integration with Expo Router and nested layouts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,3 @@
-import SidebarTabs from '@/components/ui/sidebarTabs';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
@@ -9,11 +8,14 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context';
 SplashScreen.preventAutoHideAsync(); // Preventing automatic hiding of the splash screen
 
 export default function RootLayout() {
+	// Fonts
 	const [loaded] = useFonts({
 		'Nunito-Regular': require('../assets/fonts/Nunito-Regular.ttf'),
 		'Nunito-SemiBold': require('../assets/fonts/Nunito-SemiBold.ttf'),
 		'Nunito-ExtraBold': require('../assets/fonts/Nunito-ExtraBold.ttf'),
-	}); // Fonts
+	});
+
+	const insets = useSafeAreaInsets();
 
 	useEffect(() => {
 		if (loaded) {
@@ -25,16 +27,16 @@ export default function RootLayout() {
 		return null; // Show custom loader while loading fonts
 	}
 
-	const { top } = useSafeAreaInsets(); // Statusbar height
-
 	return (
 		<>
 			<StatusBar barStyle='light-content' backgroundColor='#020303' />
-			<SafeAreaView style={{ flex: 1, paddingTop: top, backgroundColor: '#020303' }}>
-				<Stack screenOptions={{ headerShown: false, animation: 'none' }} />
-
-				{/* Tabs navigation (sidebar) */}
-				<SidebarTabs />
+			<SafeAreaView style={{ flex: 1, paddingTop: insets.top, backgroundColor: '#020303' }}>
+				<Stack
+					// Remove the header
+					screenOptions={{
+						headerShown: false,
+					}}
+				/>
 			</SafeAreaView>
 		</>
 	);
